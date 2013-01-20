484d062a2e78c702c37118fdefcab993f9a1d94a
mistaecko <mistaecko@gmail.com>
2013-01-20 23:48:21 +0700

    allow service to return empty file names (cherry-picked from e95e06b)

--- awesometts/main.py
+++ awesometts/main.py
@@ -160,7 +160,8 @@ def ATTS_Factedit_button(self):
 		srv = getService_byName(serv_list[serviceField])
 		TTS_service[srv]['filegenerator_run'](form)
 		filename = TTS_service[srv]['filegenerator_run'](form)
-		self.addMedia(filename)
+		if filename:
+			self.addMedia(filename)
 
 def ATTS_Fact_edit_setupFields(self):
 	AwesomeTTS = QPushButton(self.widget)
@@ -219,13 +220,14 @@ def generate_audio_files(factIds, frm, service, srcField_name, dstField_name):
 		
 		filename = TTS_service[service]['record'](frm, note[srcField_name])
 		
-		if frm.radioOverwrite.isChecked():
-			if frm.checkBoxSndTag.isChecked():
-				note[dstField_name] = '[sound:'+ filename +']'
+		if filename:
+			if frm.radioOverwrite.isChecked():
+				if frm.checkBoxSndTag.isChecked():
+					note[dstField_name] = '[sound:'+ filename +']'
+				else:
+					note[dstField_name] = filename
 			else:
-				note[dstField_name] = filename
-		else:
-			note[dstField_name] += ' [sound:'+ filename +']'
+				note[dstField_name] += ' [sound:'+ filename +']'
 		note.flush()
 		
 	return returnval
